59d6285ad5d0e492708ecb7daeca316ed4d511c1
Pascal Casier <pascal.casier@yahoo.com.br>
2024-08-15 07:56:13 -0300

    correction score

--- main_with_comments.gd
+++ main_with_comments.gd
@@ -1,14 +1,19 @@
 extends Control
 
-@onready var category_1: Button = %Category1
-@onready var category_2: Button = %Category2
-@onready var category_3: Button = %Category3
-@onready var category_4: Button = %Category4
-@onready var category_5: Button = %Category5
+# Références aux boutons de catégorie et au bouton d'option
+@onready var category_buttons = [
+	%Category1,
+	%Category2,
+	%Category3,
+	%Category4,
+	%Category5
+]
 @onready var option_button: OptionButton = %OptionButton
+@onready var audio_player: AudioStreamPlayer = %AudioStreamPlayer  # Référence au nœud AudioStreamPlayer existant
 
-var training : bool = false
+var training : bool = false  # Mode d'entraînement ou de jeu
 
+# Dictionnaire des catégories et des mots associés
 var categories = {
 	"ecole": ["cahier", "cartable", "chaise", "ciseaux", "colle", "crayon", "gomme", "pinceau", "regle", "table", "tableau", "taille"],
 	"corps": ["tête", "bras", "jambe", "pied"],
@@ -23,21 +28,19 @@ var categories = {
 	"jours": ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
 }
 
-var selected_category = ""
-var current_word_index = 0
-var score = 0
-var shuffled_words = []
+var selected_category = ""  # Catégorie sélectionnée
+var current_word_index = 0  # Index du mot actuel
+var score = 0  # Score du joueur
+var shuffled_words = []  # Liste des mots mélangés
 
 func _ready():
-	# Connect buttons to functions
-	category_1.pressed.connect(_on_category_selected.bind("ecole"))
-	category_2.pressed.connect(_on_category_selected.bind("corps"))
-	category_3.pressed.connect(_on_category_selected.bind("animaux"))
-	category_4.pressed.connect(_on_category_selected.bind("fruits"))
-	category_5.pressed.connect(_on_category_selected.bind("couleurs"))
+	# Connecte les boutons de catégorie aux fonctions
+	for i in range(category_buttons.size()):
+		category_buttons[i].pressed.connect(_on_category_selected.bind(categories.keys()[i]))
 	option_button.add_item("Jeu", 0)
-	option_button.add_item("entrainement", 1)
+	option_button.add_item("Entrainement", 1)
 
+# Fonction appelée lorsqu'une catégorie est sélectionnée
 func _on_category_selected(category):
 	selected_category = category
 	shuffled_words = categories[selected_category].duplicate()
@@ -46,24 +49,35 @@ func _on_category_selected(category):
 	score = 0
 	update_score()
 	if !training:
-		next_word()
 		start_game()
 	else:
 		%Training.show()
 
+# Démarre le jeu
 func start_game():
 	%Game.show()
 	current_word_index = 0
 	score = 0
+	total_words_proposed = 0  # Réinitialise le nombre total de mots proposés
 	shuffled_words = categories[selected_category].duplicate()
 	shuffled_words.shuffle()
 	update_score()
 	next_word()
 
-var repeat_count = 2
-var repeat_index = 0
-var total_words_proposed = 0
+func _on_restart_button_pressed():
+	start_game()
+	%EndGame.hide()
 
+func _on_menu_button_pressed():
+	%Game.hide()
+	%EndGame.hide()
+
+
+var repeat_count = 1  # Nombre de répétitions pour chaque mot
+var repeat_index = 0  # Compteur de répétitions pour le mot actuel
+var total_words_proposed = 0  # Nombre total de mots proposés
+
+# Passe au mot suivant
 func next_word():
 	if current_word_index < shuffled_words.size():
 		var word = shuffled_words[current_word_index]
@@ -78,23 +92,19 @@ func next_word():
 	else:
 		end_game()
 
-func update_score():
-	%Score.text = "Score : %d/%d" % [score, total_words_proposed]
-
-
+# Joue l'audio pour le mot donné
 func play_audio(word):
-	
 	var audio_path = "res://audio/%s.mp3" % word
 	var audio_stream = load(audio_path)
 	
 	if audio_stream:
-		%AudioStreamPlayer.stream = audio_stream
-		%AudioStreamPlayer.play()
+		audio_player.stop()  # Arrête l'audio en cours
+		audio_player.stream = audio_stream
+		audio_player.play()
 	else:
 		print("Erreur : Fichier audio non trouvé pour le mot %s" % word)
 
-
-
+# Affiche les options pour le mot correct
 func display_options(correct_word):
 	var options = %Options
 	for child in options.get_children():
@@ -103,7 +113,7 @@ func display_options(correct_word):
 	var words = categories[selected_category].duplicate()
 	words.erase(correct_word)
 	words.shuffle()
-	shuffled_words = words.slice(0, 3)
+	var shuffled_words = words.slice(0, 3)
 	shuffled_words.append(correct_word)
 	shuffled_words.shuffle()
 
@@ -115,14 +125,13 @@ func display_options(correct_word):
 		button.pressed.connect(_on_option_selected.bind(word, correct_word))
 		options.add_child(button)
 
-	# Add replay button
+	# Ajouter le bouton de replay
 	var replay_button = Button.new()
 	replay_button.icon = load("res://icons/play.png")
 	replay_button.pressed.connect(play_audio.bind(correct_word))
-	#replay_button.connect("pressed", self, "play_audio", [correct_word])
 	options.add_child(replay_button)
 
-
+# Fonction appelée lorsqu'une option est sélectionnée
 func _on_option_selected(selected_word, correct_word):
 	if selected_word == correct_word:
 		score += 1
@@ -137,19 +146,17 @@ func _on_option_selected(selected_word, correct_word):
 	update_score()
 	next_word()
 
+# Met à jour le score affiché
+func update_score():
+	%Score.text = "Score : %d/%d" % [score, total_words_proposed]
 
+# Termine le jeu
 func end_game():
 	%EndGame.show()
 	%endLbl.text = "Jeu terminé ! Votre score est : %d/%d" % [score, total_words_proposed]
 
-func _on_restart_button_pressed():
-	start_game()
-	%EndGame.hide()
 
-func _on_menu_button_pressed():
-	%Game.hide()
-	%EndGame.hide()
-
+# Gère la sélection du mode de jeu
 func _on_option_button_item_selected(index: int) -> void:
 	if index == 0:
 		training = false
